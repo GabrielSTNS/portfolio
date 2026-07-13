b0b63013137e5414ffe8aebcc5d9b097ee4b10c4
GabrielSTNS <gabriel2000.santana@gmail.com>
2026-07-13 14:46:33 -0300

    :sparkles: feat: Atualização no paragráfo da página de Sobre.

--- src/pages/sobre/Sobre.jsx
+++ src/pages/sobre/Sobre.jsx
@@ -9,15 +9,14 @@ const Sobre = () => {
         <div className={s.boxTexto}>
           <h1>Sobre mim</h1>
           <p>
-            Olá, meu nome é Gabriel Santana. Sou estudante do último semestre de
-            Análise e Desenvolvimento de Sistemas e Desenvolvedor Full Stack
-            formado pela Escola Vai na Web. Tenho experiência com tecnologias
-            front-end como HTML, CSS/SCSS, JavaScript e React.js, e no back-end
-            trabalho com Node.js e Express para criação de APIs RESTful, além de
-            PostgreSQL para modelagem e gerenciamento de banco de dados. Sou
-            apaixonado por construir soluções completas, do design à lógica do
-            servidor, e estou sempre em busca de novos desafios para evoluir
-            como desenvolvedor.
+            Desenvolvedor Full Stack graduado em Análise e Desenvolvimento de
+            Sistemas. Possuo sólida experiência na criação de interfaces
+            front-end modernas (HTML, CSS, JavaScript e React) e na construção
+            de APIs RESTful no back-end utilizando Node.js e Express. Tenho
+            proficiência em modelagem e gerenciamento de bancos de dados
+            relacionais com PostgreSQL. Busco integrar uma equipe inovadora, em
+            um ambiente que proporcione desafios contínuos e impulsione meu
+            desenvolvimento profissional e pessoal.
           </p>
         </div>
       </section>
